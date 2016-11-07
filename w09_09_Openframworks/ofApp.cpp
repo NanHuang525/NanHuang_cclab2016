ba54e16cf0eebeabf8d0be060a8635c73c50ffd3
NanHuang525 <huann168@newschool.edu>
2016-11-07 18:51:01 -0500

    Create ofApp.cpp

---
+++ w09_09_Openframworks/ofApp.cpp
@@ -0,0 +1,86 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(0);
+    mGravity = ofVec2f(0.f, 0.02f);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    for (int i = 0; i < mSystems.size(); i++) {
+        // apply gravity to all particles
+        mSystems[i].update(mGravity);
+    }
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    for (int i = 0; i < mSystems.size(); i++) {
+        // draw particle systems
+        mSystems[i].draw();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    cout<<key<<endl;
+    if(key==65||key==97){
+        mGravity -=ofVec2f(0.f,0.05f);
+    }else if(key==100||key==68){
+        mGravity +=ofVec2f(0.f,0.05f);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    // create a new particle system after click
+    ParticleSystem system(ofVec2f(x, y));
+    mSystems.push_back(system);
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
